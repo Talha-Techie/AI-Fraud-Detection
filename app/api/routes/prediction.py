9a15beb759a9b974f7102cd1e54303fd13a573fa
Talha-Techie <talhashoaib192@gmail.com>
2024-09-03 16:54:00 +0500

    renamed some routes

--- app/api/routes/prediction.py
+++ app/api/routes/prediction.py
@@ -2,21 +2,21 @@ import logging
 from starlette.requests import Request
 
 from fastapi import APIRouter
-from ...schemas import InputPayload, Predictions
+from ...schemas import InputPayload, PredictionResult
 from ...services import utils
 
 
 routeur = APIRouter()
 
-@routeur.post("/predict", response_model=Predictions, name="predict")
+@routeur.post("/predict", response_model=PredictionResult, name="predict")
 async def predict(request: Request, payload: InputPayload):
     logging.info("/predict endpoint call")
     
     model = request.app.state.model
     utils.check_model(model)
     
-    X = utils.preprocess_predict_payload(payload)
+    X = utils.process_predict_payload(payload)
     predictions = model.predict(X)
-    result = utils.postprocess_prediction(predictions)
+    result = utils.process_prediction_result(predictions)
     
     return result
